bd0cb174bf5bd2691371e99c259273141e675bcf
Rusiru Chathushanka <32477672+RusiruChathushanaka@users.noreply.github.com>
2023-07-02 18:13:57 +0530

    completed supabase CRUD

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,50 @@
 import supabase from "../config/superbaseClient";
+import { useEffect, useState } from "react";
+
+//components
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  const handleDelete = (id) => {
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((sm) => sm.id !== id);
+    });
+  };
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+      if (error) {
+        setFetchError("Could not fetch the smoothies.");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              ></SmoothieCard>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
